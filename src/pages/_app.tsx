625c0c49a364a19fc2874e13316969ecd438ecbb
softgenai <admin@softgen.ai>
2026-08-15 08:43:42 +0000

    feat(ui): implement light mode theme variables

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
         <CartProvider>
           <Layout>
             <Component {...pageProps} />
